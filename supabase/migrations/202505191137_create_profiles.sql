f21c165fb5dc063a57cd8a3a74f678cfe6d1bb1f
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-05-20 06:33:03 +0530

    202505191137_create_profiles.sql

--- supabase/migrations/202505191137_create_profiles.sql
+++ supabase/migrations/202505191137_create_profiles.sql
@@ -1,5 +1,2 @@
-CREATE TABLE profiles (
-  id UUID REFERENCES auth.users(id) ON DELETE CASCADE,
-  role TEXT DEFAULT 'user',
-  PRIMARY KEY (id)
-);
+ALTER TABLE profiles
+     ADD COLUMN IF NOT EXISTS role TEXT DEFAULT 'user';
